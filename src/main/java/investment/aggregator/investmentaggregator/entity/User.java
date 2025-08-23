44af5fa4f680a61e8a2220a5eed96a35c9f50476
Eduardolimzz <eduardoaluno1800@gmail.com>
2025-08-23 18:56:45 -0300

    feat: criar User entity com JPA e Lombok

---
+++ src/main/java/investment/aggregator/investmentaggregator/entity/User.java
@@ -0,0 +1,38 @@
+package investment.aggregator.investmentaggregator.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    //esse atributo da nossa classe vai ser um identificador no banco de dados, userid Primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)//cada inserção de um novo usuario vai criar automaticamente
+    private UUID userid;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updatedTimestamp;
+}
