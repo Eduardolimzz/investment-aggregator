f38ffe1e1b9675579b2cb794bc8dfbfd1fdda129
Eduardolimzz <eduardoaluno1800@gmail.com>
2025-09-01 19:14:23 -0300

    test: adiciona dothrow e asserthrow do mockito e junit

--- src/test/java/investment/aggregator/investmentaggregator/service/UserServiceTest.java
+++ src/test/java/investment/aggregator/investmentaggregator/service/UserServiceTest.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -56,6 +57,22 @@ public class UserServiceTest {
 
             //Assert:
             assertNotNull(output);
+        }
+
+        @Test
+        @DisplayName("Should throw exception when error occurs")
+        void shouldThrowExceptionWhenErroOccurs(){
+
+            doThrow(new RuntimeException()).when(userRepository).save(any());
+            var input = new CreateUserDto(
+                    "username",
+                    "email@gmail.com",
+                    "123"
+            );
+
+            //Act e Assert:
+            assertThrows(RuntimeException.class, () -> userService.createUser(input));
+
         }
     }
 }
